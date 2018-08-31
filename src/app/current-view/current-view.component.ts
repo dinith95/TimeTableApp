c47c4d5d1ca0556fef96576f528a9540d58eb1ea
dinith72 <jdinith72@gmail.com>
2018-08-31 21:03:11 +0530

    final commits to the current view

---
+++ src/app/current-view/current-view.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import {CurrentViewFormat} from '../Controller/CurrentViewDataloader';
+
+
+const timeTableData: CurrentViewFormat[] = [
+  {
+    previousSlot: {code: 'inter 12213', name: 'programming', location: 'hall01', startingTime: '8 A.M', endingTime: '9 A.M.', floor: 'f1'},
+    currentSlot: {code: 'inter 12213', name: 'coding', location: 'hall01', startingTime: '8 A.M', endingTime: '9 A.M.', floor: 'f1'} ,
+    nextSlot: {code: 'inter 2222', name: 'programming', location: 'hall01', startingTime: '8 A.M', endingTime: '9 A.M.', floor: 'f2'},
+  },
+  {
+    previousSlot: {code: 'inter 12213', name: 'programming', location: 'hall01', startingTime: '8 A.M', endingTime: '9 A.M.', floor: 'f1'},
+    currentSlot: {code: 'inter 12213', name: 'programming', location: 'hall01', startingTime: '8 A.M', endingTime: '9 A.M.', floor: 'f2'} ,
+    nextSlot: {code: 'inter 2222', name: 'programming', location: 'hall01', startingTime: '8 A.M', endingTime: '9 A.M.', floor: 'f3'},
+  },
+];
+
+@Component({
+  selector: 'app-current-view',
+  templateUrl: './current-view.component.html',
+  styleUrls: ['./current-view.component.css']
+})
+
+export class CurrentViewComponent implements OnInit {
+
+  constructor() {
+    this.time = new Date();
+  }
+
+  timeSlotPrevios: string;
+  timeslotCurrent: string;
+  timeSlotNext: string;
+  time: Date;
+  dataSource = timeTableData;
+  displayedColumns: string[] = ['previousSlot', 'currentSlot', 'nextSlot'];
+
+  ngOnInit() {
+    let time;
+    let startTime: string;
+    let endTime: string;
+    //
+    // setting the previous time
+    time = this.time.getHours() - 1;
+    if (time < 12) {
+      startTime = time + ' A.M.';
+      endTime = (time + 1) + ' A.M.';
+    } else if (time === 12) {
+      startTime = time + 'noon';
+      endTime = '1 P.M.';
+    } else {
+      startTime = (time - 12) + ' P.M.';
+      endTime = (time - 11) + ' P.M.';
+    }
+    this.timeSlotPrevios = startTime + ' - ' + endTime;
+
+    //
+    // setting the current time
+    time = this.time.getHours();
+    if (time < 12) {
+      startTime = time + ' A.M.';
+      endTime = (time + 1) + ' A.M.';
+    } else if (time === 12) {
+      startTime = time + 'noon';
+      endTime = '1 P.M.';
+    } else {
+      startTime = (time - 12) + ' P.M.';
+      endTime = (time - 11) + ' P.M.';
+    }
+    this.timeslotCurrent = startTime + ' - ' + endTime;
+
+    //
+    // setting the next time
+    time = this.time.getHours() + 1;
+    if (time < 12) {
+      startTime = time + ' A.M.';
+      endTime = (time + 1) + ' A.M.';
+    } else if (time === 12) {
+      startTime = time + ' noon';
+      endTime = '1 P.M.';
+    } else {
+      startTime = (time - 12) + ' P.M.';
+      endTime = (time - 11) + ' P.M.';
+    }
+    this.timeSlotNext = startTime + ' - ' + endTime;
+  }
+  }
+
+
+
+
+
